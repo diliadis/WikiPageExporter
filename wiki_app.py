2266865bc3da1a2864d3403e922a701ddfbb5894
Dimitrios Iliadis <jimmyhliad@gmail.com>
2022-02-28 08:34:49 +0000

    small fix

--- wiki_app.py
+++ wiki_app.py
@@ -20,7 +20,7 @@ page_wiki = wiki.page(page_name)
 if page_wiki.exists():
     st.header("Output:")
     text_to_save = os.linesep.join([s for s in page_wiki.text.splitlines() if s])
-    text_to_save = text_to_save.strip().replace("\n", "")
+    text_to_save = text_to_save.replace("\n", "")
     st.text_area("", value=text_to_save, height=500)
 
     st.download_button(
